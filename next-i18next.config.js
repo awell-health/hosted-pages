f9e0db7b4ee76a88f76ef0fa0386c630082d3fb8
Nick Hellemans <NickHellemans93@gmail.com>
2022-09-24 01:28:01 +0200

    disable automatic locale detection

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,10 @@ module.exports = {
     // These are all the locales you want to support in
     // your application
     locales: ['en', 'fr', 'nl', 'et'],
+    // When localeDetection is set to false Next.js will no longer automatically
+    // redirect based on the user's preferred locale and will only provide locale
+    // information detected from either the locale based domain or locale path as described above.
+    localeDetection: false,
     defaultLocale: 'en',
     localePath: './public/locales',
   },
