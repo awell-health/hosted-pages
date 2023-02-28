62a1ce45d245fd8f7bbd98c658d75acae55f722a
Mohsin Hayat <mohsinhayat104@gmail.com>
2023-02-28 14:44:54 +0500

    refactor(): cleanup imports in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,6 @@ import { appWithTranslation } from 'next-i18next'
 
 import type { FC, ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import { StartHostedActivitySessionFlow } from '../src/components/StartHostedActivitySessionFlow'
-import { StartHostedActivitySessionParams } from '../types'
-import { isNil } from 'lodash'
-import { ValidateAndRedirectHostedPagesLink } from '../src/components/ValidateAndRedirectHostedPagesLink'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
