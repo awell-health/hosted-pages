53fd043ab9eb88f2927511a9a64322e13c9b2513
Mohsin Hayat <mohsinhayat104@gmail.com>
2023-03-02 23:34:55 +0500

    fix(): use getStaticProps instead of getServerSideProps in hosted pages link page

--- pages/l/[hostedPagesLinkId].tsx
+++ pages/l/[hostedPagesLinkId].tsx
@@ -30,7 +30,7 @@ const HostedPagesLink: NextPage = () => {
 
 export default HostedPagesLink
 
-export async function getServerSideProps({ locale }: { locale: string }) {
+export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common'])),
