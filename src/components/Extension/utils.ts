cabe45650651786dc795c38899020c47de4bdfa3
Rahul Keerthi <rahulkeerthi2@gmail.com>
2023-03-24 18:08:16 +0000

    feat(AST-4990): use ui-lib v0.0.61 and add useSelect option to question config

--- src/components/Extension/utils.ts
+++ src/components/Extension/utils.ts
@@ -1,7 +1,7 @@
-import type { ExtensionActionField } from '../../types/generated/types-orchestration'
+import type { PluginActionField } from '../../types/generated/types-orchestration'
 
 export const mapActionFieldsToObject = <T extends Record<string, unknown>>(
-  fields: ExtensionActionField[]
+  fields: PluginActionField[]
 ): T =>
   fields.reduce((previousValue, currentValue) => {
     return {
