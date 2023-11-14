71c2620a159c509c88d25805736b58bf82be3685
Rahul Keerthi <rahul@awellhealth.com>
2023-11-14 14:13:32 +0000

    feat(sentry): add sentry breadcrumbs for slow redirects and a better expiration page (#179)
    * feat(sentry): add sentry breadcrumbs for slow redirects and a better expiration page
    
    * fix(translations): replace english translations in other languages

--- src/components/CancelPage/CancelPage.tsx
+++ src/components/CancelPage/CancelPage.tsx
@@ -4,14 +4,20 @@ import { FC } from 'react'
 import Image from 'next/image'
 import cancelIcon from './../../assets/cancel.svg'
 
-export const CancelPage: FC = (): JSX.Element => {
+interface CancelPageProps {
+  message?: string
+}
+
+export const CancelPage: FC<CancelPageProps> = ({
+  message = 'session.session_canceled',
+}): JSX.Element => {
   const { t } = useTranslation()
   return (
     <div className={classes.cancel_page}>
       <div className={classes.cancel_icon}>
         <Image src={cancelIcon} alt="" width={64} height={64} />
       </div>
-      <div className={classes.cancel_text}>{t('session.session_canceled')}</div>
+      <div className={classes.cancel_text}>{t(message)}</div>
     </div>
   )
 }
