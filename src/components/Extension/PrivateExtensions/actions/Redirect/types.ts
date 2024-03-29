755621de928b82c85b6805ffce46b1501bb47c45
Nick Hellemans <nick@awellhealth.com>
2024-03-29 14:49:28 +0100

    TP-1334: redirect & patient recommendation action (#218)
    * fix(): remove ngrok default header
    
    * feat(extensions): redirect & patient recommendation
    
    * chore(package): bump ui-lib
    
    * chore(package): updated lock file
    
    ---------
    
    Co-authored-by: Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>

---
+++ src/components/Extension/PrivateExtensions/actions/Redirect/types.ts
@@ -0,0 +1,9 @@
+export {
+  type DataPoints,
+  useCompleteExtensionActivity,
+} from '../../../../../hooks/useCompleteExtensionActivity'
+
+export type ActionFields = {
+  redirectUrl: string
+  redirectMessage?: string
+}
