08d9a8a607703306731bf61d237efb84af8cfc25
Danijel Bjelajac <danijel@awellhealth.com>
2024-04-10 16:57:26 +0200

    refactor: simplify check for active activity - remove extra functions, everything can be done is useEffect

--- src/components/Activities/context/ActivityProvider.tsx
+++ src/components/Activities/context/ActivityProvider.tsx
@@ -12,6 +12,9 @@ const POLLING_DELAY = 5000 // 5 seconds
 interface ActivityProviderProps {
   children?: React.ReactNode
 }
+
+const isActive = (activity: Activity | undefined) =>
+  activity?.status === ActivityStatus.Active
 /**
  * Provider to store of the current activity being shown to user
  * and to determine the next activity from the activities list.
@@ -22,19 +25,15 @@ export const ActivityProvider: FC<ActivityProviderProps> = ({ children }) => {
   const { activities, loading, error, refetch, startPolling, stopPolling } =
     useSessionActivities()
 
-  // determine if we have current activity set and does it have active status
-  // if so, we a waiting for stakeholder to complete the activity
-  const isCurrentActive = () => {
+  // activities list changes as we get new activities from the server or as we complete activities
+  // this useEffect drives whole AHP logic, only by activities being changed in apollo cache
+  // and base on their status do we determine what to show to the user
+  useEffect(() => {
+    // get current from the list, it may be updated
     const current = activities.find(({ id }) => id === currentActivity?.id)
-    return !isNil(current) && current.status === ActivityStatus.Active
-  }
-
-  const onActivitiesChange = () => {
-    // we don't have current activity or current is no longer active
-    if (!isCurrentActive()) {
-      const firstActive = activities.find(
-        ({ status }) => status === ActivityStatus.Active
-      )
+    // try and change current activity if it's not active
+    if (!isActive(current)) {
+      const firstActive = activities.find(isActive)
       if (isNil(firstActive)) {
         // nothing to activate, start polling for new activities so we don't rely on subscriptions
         startPolling(POLLING_DELAY)
@@ -46,13 +45,6 @@ export const ActivityProvider: FC<ActivityProviderProps> = ({ children }) => {
       // undefined will result in loading page with reload timer
       setCurrentActivity(firstActive)
     }
-  }
-
-  // activities list changes as we get new activities from the server or as we complete activities
-  // this useEffect drives whole AHP logic, only by activities being changed in apollo cache
-  // and base on their status do we determine what to show to the user
-  useEffect(() => {
-    onActivitiesChange()
   }, [activities])
 
   if (loading) {
