b69b76197f6daa77098ad0bbf9d919ca9a245d3f
ebomcke-awell <47522087+ebomcke-awell@users.noreply.github.com>
2024-05-29 11:34:43 +0200

    Create first experiment of logging to gcp from ahp (#236)
    * Integrate GCP logs
    
    * feat(monitoring): Integrate GCP logs to gather more insights about hosted sessions and their activities
    
    * fix(logging): Improve logging
    
    * feat(observability): Add more and better logs on form submission. Improve logs to make them easy to explore
    
    * fix(logging): Remove comment
    
    * fix(logging): Create a temporary file to store google cloud credentials and use regular logger when credentials are not set.
    
    ---------
    
    Co-authored-by: Flávio Ferreira <flavio@awellhealth.com>

--- src/components/Activities/ActivityFactory.tsx
+++ src/components/Activities/ActivityFactory.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Activity, ActivityObjectType } from './types'
 import { useTranslation } from 'next-i18next'
 import { Form } from '../Form'
@@ -6,9 +6,19 @@ import { Message } from '../Message'
 import { Checklist } from '../Checklist'
 import { ErrorPage } from '../ErrorPage'
 import { Extension } from '../Extension'
+import { useLogging } from '../../hooks/useLogging'
+import { LogEvent } from '../../hooks/useLogging/types'
 
 export const ActivityFactory = ({ activity }: { activity?: Activity }) => {
   const { t } = useTranslation()
+  const { infoLog } = useLogging()
+
+  useEffect(() => {
+    infoLog(
+      { msg: 'Loading new activity', activity },
+      LogEvent.ACTIVITY_LOADING
+    )
+  }, [])
 
   switch (activity?.object.type) {
     case ActivityObjectType.Form:
