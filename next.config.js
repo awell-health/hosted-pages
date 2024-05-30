a8a00724711724b2fa9fc4a3d85a18d84056d1da
Etienne Bömcke <etienne@awellhealth.com>
2024-05-30 14:14:54 +0200

    fix: use di to initialise the logger instance
    - reuse a known pattern with typedi container holding a logger
    instance configured at startup time

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const { i18n } = require('./next-i18next.config')
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
 const { withSentryConfig } = require('@sentry/nextjs')
+const { Logging } = require('@google-cloud/logging')
+const typedi = require('typedi')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -20,4 +25,23 @@ const nextPlugins = [
   (nextConfig) => withSentryConfig(nextConfig, sentryWebpackPluginOptions),
 ]
 
-module.exports = withPlugins(nextPlugins, nextConfig)
+const initialiseLogger = () => {
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS_KEY) {
+    const keyFile = path.join(os.tmpdir(), 'logging-sa-key.json')
+    fs.writeFileSync(
+      keyFile,
+      process.env.GOOGLE_APPLICATION_CREDENTIALS_KEY ?? ''
+    )
+    const gcpLoggingClient = new Logging({ keyFile })
+    const logger = gcpLoggingClient.log('hosted-sessions')
+    typedi.Container.set({ id: 'gcpLogger', value: logger })
+  }
+}
+
+module.exports = async (phase) => {
+  const config = withPlugins(nextPlugins, nextConfig)(phase, {})
+  if (['phase-production-server', 'phase-development-server'].includes(phase)) {
+    initialiseLogger()
+  }
+  return config
+}
