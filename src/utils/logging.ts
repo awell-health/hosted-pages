e5e57dd1b5b7e0bf83145d659a9a8b7efa209359
Flávio Ferreira <flavio@awellhealth.com>
2024-05-31 14:29:37 +0100

    fix(logging): Replace logging initalization strategy (#239)
    * fix(logging): Improve logging initalization to ensure we dont have exceptions happening.
    
    * fix(logging): Add new strategy to initialize google cloud logger
    
    * remove typedi

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,12 +1,38 @@
-import { Log } from '@google-cloud/logging'
-import Container from 'typedi'
+import { Log, Logging } from '@google-cloud/logging'
+import path from 'path'
+import fs from 'fs'
+import os from 'os'
 
 const metadata = {
   resource: { type: 'global' },
 }
 
+let loggerInstance: Log | undefined = undefined
+
+const getLogger = (): Log | undefined => {
+  if (!process.env.GOOGLE_APPLICATION_CREDENTIALS_KEY) {
+    console.error(
+      'Environment variable GOOGLE_APPLICATION_CREDENTIALS_KEY is not set'
+    )
+    return undefined
+  }
+
+  if (loggerInstance === undefined) {
+    console.log('Initializing logger')
+    const keyFile = path.join(os.tmpdir(), 'logging-sa-key.json')
+    fs.writeFileSync(
+      keyFile,
+      process.env.GOOGLE_APPLICATION_CREDENTIALS_KEY ?? ''
+    )
+    const gcpLoggingClient = new Logging({ keyFile })
+    loggerInstance = gcpLoggingClient.log('hosted-sessions')
+  }
+
+  return loggerInstance
+}
+
 export function log(params: {}, severity: string, error: string | {}) {
-  const logger = Container.get<Log>('gcpLogger')
+  const logger = getLogger()
   if (logger === undefined) {
     console.log('GCP log entry:', JSON.stringify({ params, severity, error }))
   } else {
