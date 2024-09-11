18c9e6e263c3bdaab8591361d9351357c3d68f75
Nick Hellemans <nick@awellhealth.com>
2024-09-11 07:07:49 +0100

    chore(): remove unused var

--- src/components/Extension/IdentityVerification/actions/DobCheck/DobCheck.tsx
+++ src/components/Extension/IdentityVerification/actions/DobCheck/DobCheck.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useMemo, useState } from 'react'
+import React, { FC, useMemo, useState } from 'react'
 import classes from './DobCheck.module.css'
 import activityClasses from '../../../../../../styles/ActivityLayout.module.css'
 
