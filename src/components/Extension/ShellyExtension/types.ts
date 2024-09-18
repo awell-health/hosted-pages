97c887c035e3af6e6d71df911541e26a3ff0ee64
Mohsin Hayat <mohsinhayat104@gmail.com>
2024-09-18 22:51:47 +0500

    Update types.ts

--- src/components/Extension/ShellyExtension/types.ts
+++ src/components/Extension/ShellyExtension/types.ts
@@ -3,17 +3,7 @@ export {
   useCompleteExtensionActivity,
 } from '../../../hooks/useCompleteExtensionActivity'
 
-export type ShellyExtensionSettings = {
-  apiKey?: string
-  environment: 'production' | 'staging'
-}
-
 export enum ActionKey {
   REVIEW_MEDICATION_EXTRACTION = 'reviewMedicationExtraction',
 }
 
-export type CompleteFlowFields = {
-  clientLabel: string
-  flowLabel: string
-  variantLabel?: string
-}
