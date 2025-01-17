658c88b21c877014658a5e400023ba4e9a3cf5a9
Flávio Ferreira <flavio@awellhealth.com>
2025-01-17 15:55:30 +0000

    Revert the change on pooling for activities and add small improvements

--- src/components/Extension/SharedActions/WaitForActivityToComplete/WaitForActivityToComplete.tsx
+++ src/components/Extension/SharedActions/WaitForActivityToComplete/WaitForActivityToComplete.tsx
@@ -1,7 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
 import type { ExtensionActivityRecord } from '../../types'
-import { useTranslation } from 'next-i18next'
 import { LoadingPage } from '../../../LoadingPage'
 import { useSessionActivities } from '../../../../hooks/useSessionActivities'
 
@@ -12,10 +11,12 @@ interface StartCareFlowAndSessionProps {
 export const WaitForActivityToComplete: FC<
   StartCareFlowAndSessionProps
 > = () => {
-  const { t } = useTranslation()
   // performing polling until the current activity is completed
   // the activity provider is the one that moves the page once the current activity changes
-  const { startPolling } = useSessionActivities()
-  startPolling(2000)
+  const { startPolling, stopPolling } = useSessionActivities()
+  useEffect(() => {
+    startPolling(2000)
+    return () => stopPolling()
+  }, [])
   return <LoadingPage />
 }
