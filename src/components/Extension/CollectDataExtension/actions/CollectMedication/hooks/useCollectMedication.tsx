8399140f9919c292b87269083b9eb75133787c9b
Nick Hellemans <nick@awellhealth.com>
2025-01-28 09:58:52 +0100

    feat(collect-meds): human readable format

--- src/components/Extension/CollectDataExtension/actions/CollectMedication/hooks/useCollectMedication.tsx
+++ src/components/Extension/CollectDataExtension/actions/CollectMedication/hooks/useCollectMedication.tsx
@@ -1,5 +1,11 @@
-import { useCallback } from 'react'
+import { useCallback, ComponentProps } from 'react'
 import { DataPoints, useCompleteExtensionActivity } from '../../../types'
+import { CollectMedication as CollectMedicationComponent } from '@awell-health/ui-library'
+import { isEmpty } from 'lodash'
+
+type MedicationData = Parameters<
+  ComponentProps<typeof CollectMedicationComponent>['onSubmit']
+>[0]
 
 export const useCollectMedication = () => {
   const { isSubmitting, onSubmit: _onSubmit } = useCompleteExtensionActivity()
@@ -10,11 +16,26 @@ export const useCollectMedication = () => {
       medicationData,
     }: {
       activityId: string
-      medicationData: string
+      medicationData: MedicationData
     }) => {
+      const prettyMedicationData = medicationData
+        .map((medication) => {
+          const parts = [medication.name]
+          if (!isEmpty(medication.dose)) parts.push(medication.dose)
+          if (
+            !isEmpty(medication.instructions) &&
+            medication.instructions !== undefined
+          )
+            parts.push(medication.instructions)
+
+          return parts.join('; ')
+        })
+        .join('\n')
+
       const dataPoints: DataPoints = [
-        { key: 'medicationData', value: medicationData }, // JSON data point value type
-        { key: 'medicationDataString', value: medicationData }, // String data point value type
+        { key: 'medicationData', value: JSON.stringify(medicationData) }, // JSON data point value type
+        { key: 'medicationDataString', value: JSON.stringify(medicationData) }, // String data point value type
+        { key: 'prettyMedicationData', value: prettyMedicationData },
       ]
 
       return _onSubmit(activityId, dataPoints)
